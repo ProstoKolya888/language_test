c849886979f3364b1adcb4aa34f1851d56389c55
ProstoKolya888 <105746592+ProstoKolya888@users.noreply.github.com>
2022-05-23 22:14:01 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,8 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_item_has_add_to_cart_button(browser):
+    browser.get(link)
+    button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
+    assert button, "Should be a button"
+
